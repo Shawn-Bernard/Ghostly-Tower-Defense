f96db638b429d3b38f14732392762d8899ef1c1d
Shawn Bernard <ssbernard@outlook.com>
2026-09-23 19:40:15 -0300

    Added Sound Manager (not tested)

---
+++ Assets/Scripts/Managers/Sound Destroyer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class SoundDestroyer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float clipLength;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator PlayAudio()
+    {
+        clipLength = audioSource.clip.length;
+
+        yield return new WaitForSeconds(clipLength);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Managers/Sound Manager.cs
+++ Assets/Scripts/Managers/Sound Manager.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+[CreateAssetMenu(menuName = "Sound Manager", fileName = "Sound Manager")]
+public class SoundManager : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static SoundManager instance;
+
+    public static SoundManager Instance
     {
-        
+        get 
+        { 
+            if (instance == null)
+            {
+                instance = Resources.Load<SoundManager>("Sound Manager");
+            }
+            return instance; 
+        }
+    }
+    public AudioSource soundObject;
+
+    private static float volumeChangeMultiplier = 0.15f;
+    private static float pitchChangeMultiplier = 0.1f;
+
+    public static void PlaySoundClip(AudioClip clip, Vector2 soundPosition,float volume)
+    {
+        float randomVolume = Random.Range(volume - volumeChangeMultiplier, volume + volumeChangeMultiplier);
+        float randomPitch = Random.Range(1 - pitchChangeMultiplier, 1 + pitchChangeMultiplier);
+
+        AudioSource audioSource = Instantiate(instance.soundObject, soundPosition, Quaternion.identity);
+
+        audioSource.clip = clip;
+        audioSource.volume = randomVolume;
+        audioSource.pitch = randomPitch;
+        audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void PlaySoundClip(AudioClip[] clips, Vector2 soundPosition, float volume)
     {
-        
+        int randomClip = Random.Range(0, clips.Length);
+        float randomVolume = Random.Range(volume - volumeChangeMultiplier, volume + volumeChangeMultiplier);
+        float randomPitch = Random.Range(1 - pitchChangeMultiplier, 1 + pitchChangeMultiplier);
+
+        AudioSource audioSource = Instantiate(instance.soundObject, soundPosition, Quaternion.identity);
+
+        audioSource.clip = clips[randomClip];
+        audioSource.volume = randomVolume;
+        audioSource.pitch = randomPitch;
+        audioSource.Play();
     }
 }
