ac5bbd3519c9ed36aba37ff93517d35ecc630dc9
Shawn Bernard <ssbernard@outlook.com>
2026-09-25 14:21:08 -0300

    Fixed Laser tower aiming

--- Assets/Scripts/Weapons/Laser Tower.cs
+++ Assets/Scripts/Weapons/Laser Tower.cs
@@ -10,18 +10,17 @@ public class LaserTower : Tower
         if (target == null || !isPlaced)
             return;
 
-        RaycastHit2D[] hits = Physics2D.RaycastAll(
+        Collider2D[] hits = Physics2D.OverlapAreaAll(
             transform.position,
-            transform.up,
-            range
+            direction * range
         );
-
+        
         foreach (var hit in hits)
         {
-            if (hit.collider == null) continue;
+            if (hit == null) continue;
 
-            if (hit.collider.TryGetComponent<IDamageable>(out IDamageable damageable) && 
-                hit.collider.CompareTag("Enemy"))// Just in case it hits the player base
+            if (hit.TryGetComponent<IDamageable>(out IDamageable damageable) && 
+                hit.CompareTag("Enemy"))// Just in case it hits the player base
             {
                 damageable.TakeDamage(damageAmount);
             }
@@ -30,13 +29,4 @@ public class LaserTower : Tower
         
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-
-        Gizmos.DrawLine(
-            transform.position,
-            transform.position + transform.up * range
-        );
-    }
 }
