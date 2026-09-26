190e72636b81428b7dd9eee7954bab07de3a34ac
Shawn Bernard <ssbernard@outlook.com>
2026-09-26 16:18:24 -0300

    Added background music for Main Menu, Gameplay, Win/Loss. Added SFX for ghost & player death, Range Tower Shoot

--- Assets/Scripts/Managers/Sound Manager.cs
+++ Assets/Scripts/Managers/Sound Manager.cs
@@ -1,9 +1,23 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
+    [SerializeField] private AudioSource backgroundMusic;
+
+    [SerializeField] private AudioClip mainMenuMusic;
+    [SerializeField] private AudioClip gameplayMusic;
+    [SerializeField] private AudioClip endGameMusic;
+
+    [Range(0,1)]
+    [SerializeField] private float backGroundMusicVolume;
+
+    [SerializeField] private GameState mainMenu;
+    [SerializeField] private GameState gameplayState;
+    [SerializeField] private GameState gameOverState;
+    [SerializeField] private GameState winnerState;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -14,6 +28,52 @@ public class SoundManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
+    private void Start()
+    {
+        backgroundMusic ??= GetComponent<AudioSource>();
+    }
+
+    public void SwitchBackgroundMusic(AudioClip musicClip)
+    {
+        backgroundMusic.clip = musicClip;
+        backgroundMusic.loop = true;
+        backgroundMusic.volume = backGroundMusicVolume;
+        backgroundMusic.Play();
+    }
+
+    public void PlayMainMenu()
+    {
+        SwitchBackgroundMusic(mainMenuMusic);
+    }
+
+    public void PlayGameplay()
+    {
+        SwitchBackgroundMusic(gameplayMusic);
+    }
+
+    public void PlayEndGame()
+    {
+        SwitchBackgroundMusic(endGameMusic);
+    }
+
+    private void OnEnable()
+    {
+        mainMenu.onEnterState += PlayMainMenu;
+        gameplayState.onEnterState += PlayGameplay;
+        gameOverState.onEnterState += PlayEndGame;
+        winnerState.onEnterState += PlayEndGame;
+    }
+
+    private void OnDisable()
+    {
+        mainMenu.onEnterState -= PlayMainMenu;
+        gameplayState.onEnterState -= PlayGameplay;
+        gameOverState.onEnterState -= PlayEndGame;
+        winnerState.onEnterState -= PlayEndGame;
+    }
+
+    #region SFX Logic
     /// <summary>
     /// Plays a audio clip at a position with volume
     /// </summary>
@@ -44,4 +104,5 @@ public class SoundManager : MonoBehaviour
 
         AudioSource.PlayClipAtPoint(clip, spawnPosition, volume);
     }
+    #endregion
 }
